bbb9c1bee213838dbe4f2ec1d320f61256c225cd
priestalex76 <priestalex@yandex.ru>
2023-09-29 20:10:09 +0600

    Google test added

--- test_version.cpp
+++ test_version.cpp
@@ -1,10 +1,12 @@
-#define BOOST_TEST_MODULE test_version
+//#define BOOST_TEST_MODULE test_version
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(test_version)
+//#include <boost/test/unit_test.hpp>
+
+/*BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
@@ -12,3 +14,16 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+*/
+
+TEST(test, ver)
+{
+// Arrange
+    //int v = version();
+
+// Act (empty for this test)
+
+// Assert
+    ASSERT_GT(version(), 0);
+    //ASSERT_TRUE(v==0);
+}
